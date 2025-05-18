6d66e141b346d3edaf65fa0ccf44cc04da8efc92
litwak913 <litwak913@qq.com>
2025-05-18 20:44:30 +0800

    chore: style

--- core/src/cn/harryh/arkpets/audio/AudioPlayer.java
+++ core/src/cn/harryh/arkpets/audio/AudioPlayer.java
@@ -13,24 +13,24 @@ import java.util.HashMap;
 
 
 public class AudioPlayer {
-    private boolean disposed;
     private final Music music;
-    private final HashMap<String,Float> startTime = new HashMap<>();
-    private final HashMap<String,Float> durationTime = new HashMap<>();
+    private final HashMap<String, Float> startTime = new HashMap<>();
+    private final HashMap<String, Float> durationTime = new HashMap<>();
     private final Timer pauseTimer = new Timer();
+    private boolean disposed;
 
     private AudioPlayer(VoiceItem group, File audio) {
         for (int i = 0; i < group.clips().size(); i++) {
             VoiceItem.VoiceClip clip = group.clips().get(i);
             VoiceItem.VoiceClip nextClip = null;
             if (i < group.clips().size() - 1) {
-                nextClip = group.clips().get(i+1);
+                nextClip = group.clips().get(i + 1);
             }
             startTime.put(clip.name(), clip.start());
             if (nextClip != null) {
-                durationTime.put(clip.name(), nextClip.start()-clip.start());
+                durationTime.put(clip.name(), nextClip.start() - clip.start());
             } else {
-                durationTime.put(clip.name(), group.duration()-clip.start());
+                durationTime.put(clip.name(), group.duration() - clip.start());
             }
         }
         music = Gdx.audio.newMusic(Gdx.files.absolute(audio.getAbsolutePath()));
@@ -41,9 +41,9 @@ public class AudioPlayer {
         return new AudioPlayer(group.getVariation(lang), oggPath);
     }
 
-    public void playAudio(String name,float pan,float vol) {
+    public void playAudio(String name, float pan, float vol) {
         if (disposed) throw new IllegalStateException();
-        float start,duration;
+        float start, duration;
         start = startTime.get(name);
         duration = durationTime.get(name);
         Logger.debug("Audio",
@@ -52,7 +52,7 @@ public class AudioPlayer {
         );
         music.play();
         music.setPosition(start);
-        music.setPan(pan,vol);
+        music.setPan(pan, vol);
         pauseTimer.scheduleTask(new Timer.Task() {
             @Override
             public void run() {
