a7becf79bd6cb9604a87071b14c15cf4cab97fc8
litwak913 <litwak913@163.com>
2025-06-22 15:00:59 +0800

    refactor: wayland client and region

--- core/src/cn/harryh/arkpets/natives/WaylandClient.java
+++ core/src/cn/harryh/arkpets/natives/WaylandClient.java
@@ -18,6 +18,26 @@ public interface WaylandClient extends Library {
 
     void wl_proxy_destroy(Pointer compositor);
 
+    default void wl_region_destroy(Pointer wl_region) {
+        wl_proxy_marshal_flags(wl_region, 0, null, wl_proxy_get_version(wl_region), 1);
+    }
+
+    default void wl_surface_set_input_region(Pointer wl_surface, Pointer wl_region) {
+        wl_proxy_marshal_flags(wl_surface, 5, null, wl_proxy_get_version(wl_surface), 0, wl_region);
+    }
+
+    default void wl_surface_commit(Pointer wl_surface) {
+        wl_proxy_marshal_flags(wl_surface, 6, null, wl_proxy_get_version(wl_surface), 0);
+    }
+
+    default Pointer wl_compositor_create_region(Pointer wl_compositor) {
+        return wl_proxy_marshal_flags(wl_compositor, 1, WaylandInterface.wl_region_interface, wl_proxy_get_version(wl_compositor), 0, (Object) null);
+    }
+
+    default Pointer wl_display_get_registry(Pointer wl_display) {
+        return wl_proxy_marshal_flags(wl_display, 1, WaylandInterface.wl_registry_interface, wl_proxy_get_version(wl_display), 0, (Object) null);
+    }
+
     @Structure.FieldOrder({"global", "global_remove"})
     class wl_registry_listener extends Structure {
         public GlobalCallback global;
--- core/src/cn/harryh/arkpets/natives/WaylandHelper.java
+++ core/src/cn/harryh/arkpets/natives/WaylandHelper.java
@@ -1,7 +1,6 @@
 package cn.harryh.arkpets.natives;
 
 import cn.harryh.arkpets.utils.Logger;
-import com.sun.jna.NativeLibrary;
 import com.sun.jna.Pointer;
 import org.lwjgl.glfw.GLFWNativeWayland;
 
@@ -11,25 +10,16 @@ public class WaylandHelper {
     private static boolean unavailable;
     private static WaylandClient.wl_registry_listener listener; // Prevent callback
     private static Pointer compositor;
-    private static int compositorVer;
-
-    private static Pointer wl_compositor_interface;
-    private static Pointer wl_region_interface;
+    private static Pointer region;
 
     private static void init() {
-        NativeLibrary lib = NativeLibrary.getInstance("wayland-client");
-        Pointer wl_registry_interface = lib.getGlobalVariableAddress("wl_registry_interface");
-        wl_compositor_interface = lib.getGlobalVariableAddress("wl_compositor_interface");
-        wl_region_interface = lib.getGlobalVariableAddress("wl_region_interface");
         Pointer display = new Pointer(GLFWNativeWayland.glfwGetWaylandDisplay());
-        int ver = WaylandClient.INSTANCE.wl_proxy_get_version(display);
-        Pointer reg = WaylandClient.INSTANCE.wl_proxy_marshal_flags(display, 1, wl_registry_interface, ver, 0, (Object) null);
+        Pointer reg = WaylandClient.INSTANCE.wl_display_get_registry(display);
         listener = new WaylandClient.wl_registry_listener();
         listener.global = (data, registry, name, iface, version) -> {
             if (iface.equals("wl_compositor")) {
                 compositor = WaylandClient.INSTANCE.wl_proxy_marshal_flags(registry, 0,
-                        wl_compositor_interface, 4, 0, name, "wl_compositor", 4, null);
-                compositorVer = WaylandClient.INSTANCE.wl_proxy_get_version(compositor);
+                        WaylandInterface.wl_compositor_interface, version, 0, name, "wl_compositor", version, null);
             }
         };
         listener.global_remove = (data, registry, name) -> {
@@ -47,22 +37,19 @@ public class WaylandHelper {
             unavailable = true;
             return;
         }
+        region = WaylandClient.INSTANCE.wl_compositor_create_region(compositor);
         ready = true;
     }
 
     public static void setTransparent(Pointer surface, boolean enable) {
         if (unavailable) return;
         if (!ready) init();
-        int surfaceVer = WaylandClient.INSTANCE.wl_proxy_get_version(surface);
         if (enable) {
-            Pointer region = WaylandClient.INSTANCE.wl_proxy_marshal_flags(compositor, 1, wl_region_interface, compositorVer, 0, (Object) null);
-            int regionVer = WaylandClient.INSTANCE.wl_proxy_get_version(region);
-            WaylandClient.INSTANCE.wl_proxy_marshal_flags(surface, 5, null, surfaceVer, 0, region);
-            WaylandClient.INSTANCE.wl_proxy_marshal_flags(surface, 6, null, surfaceVer, 0);
-            WaylandClient.INSTANCE.wl_proxy_marshal_flags(region, 0, null, regionVer, 1);
+            WaylandClient.INSTANCE.wl_surface_set_input_region(surface, region);
+            WaylandClient.INSTANCE.wl_surface_commit(surface);
         } else {
-            WaylandClient.INSTANCE.wl_proxy_marshal_flags(surface, 5, null, surfaceVer, 0, (Object) null);
-            WaylandClient.INSTANCE.wl_proxy_marshal_flags(surface, 6, null, surfaceVer, 0);
+            WaylandClient.INSTANCE.wl_surface_set_input_region(surface, null);
+            WaylandClient.INSTANCE.wl_surface_commit(surface);
         }
     }
 }
