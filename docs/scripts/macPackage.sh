d5c88a3ec5b86593286c82da444b248ad4c28e1b
litwak913 <litwak913@163.com>
2025-07-06 22:53:56 +0800

    build: add dmg build

---
+++ docs/scripts/macPackage.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo $1
+cp -v ./docs/scripts/macLaunch.sh ./desktop/build/jpackage/ArkPets.app/Contents/MacOS/ArkPets
+
+create-dmg --volname ArkPets \
+--app-drop-link 300 50 \
+--icon "ArkPets.app" 100 50 \
+--icon "LICENSE" 500 50 \
+--window-size 700 60 \
+--icon-size 100 \
+"./desktop/build/dist/ArkPets-$1-mac-$(uname -m).dmg" ./desktop/build/jpackage/
