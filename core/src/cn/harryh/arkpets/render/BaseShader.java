2951081b4695f9323de0e410619309eac6c450a9
Harry Huang <harryhuang2652@qq.com>
2026-07-04 21:27:43 +0800

    refactor(shader): simplify compile error log

--- core/src/cn/harryh/arkpets/render/BaseShader.java
+++ core/src/cn/harryh/arkpets/render/BaseShader.java
@@ -3,7 +3,6 @@
  */
 package cn.harryh.arkpets.render;
 
-import cn.harryh.arkpets.utils.Logger;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 
@@ -12,10 +11,9 @@ public class BaseShader extends ShaderProgram {
     public BaseShader(String vertexShader, String fragmentShader) {
         super(Gdx.files.internal(vertexShader), Gdx.files.internal(fragmentShader));
         if (!isCompiled()) {
-            Logger.error("Shader", "Shader program failed to compile.");
-            Logger.error("Shader", "Shader source: " + vertexShader + " & " + fragmentShader);
-            Logger.error("Shader", "Shader log: " + getLog());
-            throw new RuntimeException("Launch ArkPets failed, failed to compile shaders.");
+            throw new RuntimeException(
+                    "Failed to compile shaders, source: " + vertexShader + " & " + fragmentShader + ", log: " + getLog()
+            );
         }
     }
 }
