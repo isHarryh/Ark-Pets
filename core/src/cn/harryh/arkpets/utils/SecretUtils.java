ec33d13b433f8712c1757c8013188537533fc90f
Harry Huang <harryhuang2652@qq.com>
2026-08-16 01:24:30 +0800

    refactor: improve cdk decryption

--- core/src/cn/harryh/arkpets/utils/SecretUtils.java
+++ core/src/cn/harryh/arkpets/utils/SecretUtils.java
@@ -11,6 +11,7 @@ import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
@@ -20,60 +21,102 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
-import java.util.Base64;
-import java.util.Enumeration;
+import java.util.*;
 
 
 public class SecretUtils {
     public interface SecretAlgorithm {
         String encrypt(String plaintext) throws GeneralSecurityException;
+
         String decrypt(String encoded) throws GeneralSecurityException;
     }
 
 
     public static class WeakEncryptionV0 implements SecretAlgorithm {
-        private final String deviceBinding;
         private static final Charset charset = StandardCharsets.UTF_8;
+        private static final int MAX_KEY_CANDIDATES = 64;
 
-        public WeakEncryptionV0() {
-            deviceBinding = getDeviceBindingKey();
-        }
+        public static class DeviceBindingKeyGenerator implements Iterator<String> {
+            private final HashMap<NetworkInterface, Byte> niScoreMap;
+            private final Iterator<NetworkInterface> niIter;
+            private final String hostname;
 
-        private static String getPrimaryMacAddress() {
-            try {
-                Enumeration<NetworkInterface> eni = NetworkInterface.getNetworkInterfaces();
-                while (eni.hasMoreElements()) {
-                    NetworkInterface ni = eni.nextElement();
-                    if (ni.isUp() && !ni.isLoopback() && !ni.isVirtual()) {
-                        byte[] mac = ni.getHardwareAddress();
-                        if (mac != null) {
-                            StringBuilder sb = new StringBuilder();
-                            for (byte b : mac)
-                                sb.append(String.format("%02X:", b));
-                            return sb.substring(0, sb.length() - 1);
-                        }
-                    }
+            public DeviceBindingKeyGenerator() {
+                this.niScoreMap = new HashMap<>();
+
+                ArrayList<NetworkInterface> niList;
+                try {
+                    niList = Collections.list(NetworkInterface.getNetworkInterfaces());
+                    niList.sort(Comparator.comparingInt(ni -> scoreNetworkInterface((NetworkInterface) ni)).reversed());
+                } catch (Exception e) {
+                    niList = new ArrayList<>();
                 }
-            } catch (Exception ignored) {
-            }
-            return "unknown_mac";
-        }
+                niList.add(null);
+                this.niIter = niList.iterator();
 
-        private static String getHostname() {
-            try {
-                return InetAddress.getLocalHost().getHostName();
-            } catch (UnknownHostException ignored) {
+                String hostname;
+                try {
+                    hostname = InetAddress.getLocalHost().getHostName();
+                } catch (UnknownHostException e) {
+                    hostname = "unknown_hostname";
+                }
+                this.hostname = hostname;
             }
-            return  "unknown_hostname";
-        }
 
-        private static String getDeviceBindingKey() {
-            return System.getProperty("user.name") + ","
-                    + System.getProperty("user.home") + ";"
-                    + getHostname() + ","
-                    + getPrimaryMacAddress() + ";"
-                    + System.getProperty("os.name") + ","
-                    + System.getProperty("os.arch") + ";";
+            private byte scoreNetworkInterface(NetworkInterface ni) {
+                if (ni == null)
+                    return 0;
+                if (niScoreMap.containsKey(ni))
+                    return niScoreMap.get(ni);
+
+                byte score;
+                try {
+                    score = (byte) (1 + (ni.isUp() ? 1 : 0) + (ni.isLoopback() ? 0 : 1) + (ni.isVirtual() ? 0 : 1));
+                } catch (SocketException e) {
+                    score = 0;
+                }
+                niScoreMap.put(ni, score);
+                return score;
+            }
+
+            private static String formatMacAddress(NetworkInterface ni) {
+                if (ni == null)
+                    return null;
+
+                try {
+                    byte[] mac = ni.getHardwareAddress();
+                    if (mac == null)
+                        return null;
+                    StringBuilder sb = new StringBuilder();
+                    for (byte b : mac)
+                        sb.append(String.format("%02X:", b));
+                    return sb.substring(0, sb.length() - 1);
+                } catch (SocketException e) {
+                    return null;
+                }
+            }
+
+            @Override
+            public boolean hasNext() {
+                return niIter.hasNext();
+            }
+
+            @Override
+            public String next() {
+                if (!hasNext())
+                    throw new NoSuchElementException();
+
+                String macAddress = formatMacAddress(niIter.next());
+                if (macAddress == null)
+                    macAddress = "unknown_mac";
+
+                return System.getProperty("user.name") + ","
+                        + System.getProperty("user.home") + ";"
+                        + hostname + ","
+                        + macAddress + ";"
+                        + System.getProperty("os.name") + ","
+                        + System.getProperty("os.arch") + ";";
+            }
         }
 
         private static SecretKey generateKey(String passphrase, byte[] salt) {
@@ -96,7 +139,8 @@ public class SecretUtils {
                 byte[] iv = SecureRandom.getInstanceStrong().generateSeed(12);
 
                 Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
-                SecretKey key = generateKey(deviceBinding, salt);
+                Iterator<String> dbkGen = new DeviceBindingKeyGenerator();
+                SecretKey key = generateKey(dbkGen.next(), salt); // Use the first dbk
                 GCMParameterSpec spec = new GCMParameterSpec(128, iv);
                 cipher.init(Cipher.ENCRYPT_MODE, key, spec);
 
@@ -109,7 +153,7 @@ public class SecretUtils {
 
                 return Base64.getEncoder().encodeToString(buffer.array());
 
-            } catch (NoSuchAlgorithmException e){
+            } catch (NoSuchAlgorithmException e) {
                 throw new RuntimeException(e);
             } catch (GeneralSecurityException e) {
                 throw new GeneralSecurityException("Encryption failed");
@@ -118,29 +162,44 @@ public class SecretUtils {
 
         @Override
         public String decrypt(String encoded) throws GeneralSecurityException {
+            byte[] data = Base64.getDecoder().decode(encoded);
+            ByteBuffer buffer = ByteBuffer.wrap(data);
+
+            byte[] salt = new byte[16];
+            byte[] iv = new byte[12];
+            byte[] ciphertext = new byte[data.length - 16 - 12];
+            buffer.get(salt);
+            buffer.get(iv);
+            buffer.get(ciphertext);
+
+            Set<String> tried = new HashSet<>();
+            Iterator<String> dbkGen = new DeviceBindingKeyGenerator();
+            while (dbkGen.hasNext()) {
+                String dbk = dbkGen.next();
+                if (tried.contains(dbk))
+                    continue;
+                String plaintext = decryptWith(dbk, salt, iv, ciphertext);
+                if (plaintext != null)
+                    return plaintext;
+                tried.add(dbk);
+                if (tried.size() >= MAX_KEY_CANDIDATES)
+                    break;
+            }
+
+            throw new GeneralSecurityException("Decryption failed");
+        }
+
+        private String decryptWith(String dbk, byte[] salt, byte[] iv, byte[] ciphertext) {
             try {
-                byte[] data = Base64.getDecoder().decode(encoded);
-                ByteBuffer buffer = ByteBuffer.wrap(data);
-
-                byte[] salt = new byte[16];
-                byte[] iv = new byte[12];
-                byte[] ciphertext = new byte[data.length - 16 - 12];
-                buffer.get(salt);
-                buffer.get(iv);
-                buffer.get(ciphertext);
-
                 Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
-                SecretKey key = generateKey(deviceBinding, salt);
+                SecretKey key = generateKey(dbk, salt);
                 GCMParameterSpec spec = new GCMParameterSpec(128, iv);
                 cipher.init(Cipher.DECRYPT_MODE, key, spec);
-
-                byte[] plaintext = cipher.doFinal(ciphertext);
-                return new String(plaintext, charset);
-
-            } catch (NoSuchAlgorithmException e){
+                return new String(cipher.doFinal(ciphertext), charset);
+            } catch (NoSuchAlgorithmException e) {
                 throw new RuntimeException(e);
             } catch (GeneralSecurityException e) {
-                throw new GeneralSecurityException("Decryption failed");
+                return null;
             }
         }
     }
